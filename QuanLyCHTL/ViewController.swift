939603022b2fe229b7d365856fe9e052f8c4c5eb
thanh <thanh160523@gmail.com>
2023-05-29 21:06:42 +0700

    push

---
+++ QuanLyCHTL/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  QuanLyCHTL
+//
+//  Created by CNTT on 5/29/23.
+//  Copyright © 2023 fit.tdc. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print("abc")
+    }
+
+
+}
+
